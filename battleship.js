47838617c4f6db0e2580bc8c68d3d9fa38b7a33c
savemysoul382 <serjke88@gmail.com>
2017-07-24 16:01:23 +0300

    Add Model object -ships property -fire method, draft of hit and miss realization work continues

--- battleship.js
+++ battleship.js
@@ -1,13 +1,15 @@
+// View
+
 var view = {
-    displayMessage: function(msg) {
+    displayMessage: function (msg) {
         var messageArea = document.getElementById("messageArea");
         messageArea.innerHTML = msg;
     },
-    displayHit: function(location) {
+    displayHit: function (location) {
         var cell = document.getElementById(location);
         cell.setAttribute("class", "hit");
     },
-    displayMiss: function(location) {
+    displayMiss: function (location) {
         var cell = document.getElementById(location);
         cell.setAttribute("class", "miss");
     }
@@ -21,3 +23,30 @@ view.displayMiss("25");
 view.displayHit("26");
 
 view.displayMessage("Tap Tap Tap");
+
+// Model
+
+var model = {
+    boardSize: 7,
+    numShips: 3,
+    shipLength: 3,
+    shipSunk: 0,
+
+    ships: [{locations: ["10", "20", "30"], hits: ["", "", ""]},
+        {locations: ["32", "33", "34"], hits: ["", "", ""]},
+        {locations: ["63", "64", "65"], hits: ["", "", "hit"]}],
+
+    // Method fire, hit or miss
+    fire: function (guess) {
+        for (var i = 0; i < this.numShips.length; i++) {
+            var ship = this.ships[i];
+            var locations = ship.locations;
+            var index = locations.indexOf(guess);
+            if (index >= 0) {                             //HIT
+                ship.hits[index] = "hit";
+                return true
+            }
+        }
+        return false;                                     //MISS
+    }
+};
